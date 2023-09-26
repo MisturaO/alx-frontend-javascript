fbf62cc161d80b6a6a1f546a760c294965762ef1
MisturaO <misturaojulari91@gmail.com>
2023-09-26 13:28:56 +0100

    For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const idx of array) {
+    array = appendString + idx;
+    newArray.push(array);
+  }
+
+  return newArray;
+}
