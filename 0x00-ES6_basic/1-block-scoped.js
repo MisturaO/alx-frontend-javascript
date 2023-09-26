700ab09bae67f1294a20f6f2c52aba0d3e28c63b
MisturaO <misturaojulari91@gmail.com>
2023-09-26 02:20:44 +0100

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-   const task = false;
-   const task2 = true;
+   var task = false;
+   var task2 = true;
 
   if (trueOrFalse) {
-     return [task, task2];
+    /* eslint-disable */
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
