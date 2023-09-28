ddfcf3d680145aeeb14f2ae10afd494b80181a54
MisturaO <misturaojulari91@gmail.com>
2023-09-28 04:09:39 +0100

    Using three handlers with a promise function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  const obj = {
-    status: 200,
-    body: 'Success',
-  };
-
   return promise
-    .then(() => obj) // Resolved promise result
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    })) // Resolved promise result
     .catch(() => new Error()) // Catch error when promise rejects
     .finally(() => console.log('Got a response from the API')); // Finally returns no matter what happens
 }
