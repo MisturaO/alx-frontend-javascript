3bea07a904673ebc5c0daf7d9be50069db9a6b1e
MisturaO <misturaojulari91@gmail.com>
2023-09-28 13:25:32 +0100

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
+    .allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName),
+    ]).then((values) => {
       const arr = [];
       for (const value of values) {
         arr.push({ status: value.status, value: value.value || value.reason });
