09e4aecbe4b7e79032649b8c597c15c77c893c25
MisturaO <misturaojulari91@gmail.com>
2023-09-28 13:03:17 +0100

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      const arr = [];
+      for (const value of values) {
+        arr.push({ status: value.status, value: value.value || value.reason });
+      }
+      return arr;
+    });
+}
