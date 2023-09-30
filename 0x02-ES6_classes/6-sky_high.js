d817effd71a345667a7649bd5c24e565ac6b3bea
MisturaO <misturaojulari91@gmail.com>
2023-09-30 16:38:36 +0100

    inheriting from abstract class

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,21 +1,20 @@
 import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof floors !== 'number') {
-      throw new TypeError('floors must be a number');
-    }
-    this._floors = floors;
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
+  set floors(value) {
+    this._floors = value;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
-
-export default SkyHighBuilding;
