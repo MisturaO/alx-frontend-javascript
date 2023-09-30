4e2313987aacef4dd328b9f0c66ea1345f0ee8df
MisturaO <misturaojulari91@gmail.com>
2023-09-30 15:02:22 +0100

    Abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,29 @@
+/* An abstract class that makes sure any class that extends from it should
+implement a method named evacuationWarningMessage or throw an error if they don't
+*/
+
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+
+    /*
+        Checks if the constructor of the instance (this.constructor) is not the
+        Building class itself, and if the 'evacuationWarningMessage' method is a function.
+        If these conditions are not met, it throws an error, enforcing that subclasses must
+        override the evacuationWarningMessage method.
+        */
+    if (this.constructor !== Building
+            && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
