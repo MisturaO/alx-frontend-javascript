0cb0c679fbad38c587288a24ff0f3bd2424abd68
MisturaO <misturaojulari91@gmail.com>
2023-09-30 21:09:04 +0100

    Assigns a tag to Airport object

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,23 @@
+class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  /* This provide a more informative label or identifier(or tag) for that object
+  (i.e instance of this class), Whe we want to identify what object it is durig
+  debugging instead of getting [object object] we will get [object string_representation] 
+  which the string retured i this get method
+  */
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
+
+export default Airport;
