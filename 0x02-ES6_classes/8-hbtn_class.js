62c2be58eafd227ebfab03b8e73c394cd233e8c9
MisturaO <misturaojulari91@gmail.com>
2023-10-01 12:28:15 +0100

    Primitive data type conversion

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,16 +1,21 @@
-export default class HolbertonClass{
-    constructor(size, location){
-        this._size = size;
-        this._location = location;
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('size must be a Number');
+    } else if (typeof location !== 'string') {
+      throw new TypeError('location must be a String');
     }
+    this._size = size;
+    this._location = location;
+  }
 
-    // Implements custom type conversion to primitive data type (here it's to Number)
-    valueOf(){
-        return this._size;
-    }
+  // Implements custom type conversion to primitive data type (here it's to Number)
+  valueOf() {
+    return this._size;
+  }
 
-    // Implements custom type conversion to primitive data type (here it's to String)
-    toString(){
-        return this._location;
-    }
+  // Implements custom type conversion to primitive data type (here it's to String)
+  toString() {
+    return this._location;
+  }
 }
