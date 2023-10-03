bbf3c5982204f20d92273ca0ed18f2f19eb3c5a9
MisturaO <misturaojulari91@gmail.com>
2023-10-04 00:23:10 +0100

    Manipulate buffer data with Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,30 @@
 export default function createInt8TypedArray(length, position, value) {
+/* checks if the position is valid:
+    The an index less than zero or ggreater than or equal to lenght isn't
+    valid(i.e. if the value of length is 5, the available index would be
+    0,1,2,3,4 and not 0,1,2,3,4,5 or -1,0,1,2,3,4,5)
+*/
   if (position < 0 || position >= length) {
     return 'Position outside range';
   }
 
-  // Allocates memory space to store and maipulate data(). Basically Just a chunk of raw memory.
+  /* Allocates memory space to store and maipulate data(). Basically Just a chunk of raw memory.
+  DataView object is used to create a view over an existing ArrayBuffer, providing a structured
+  way to interact with the binary data stored in the ArrayBuffer, so we can have access to the
+  '.set()' and '.get()' methods of the Dataview object. These methods allow us to read/write
+  values of various data types at specific byte positions within the allocated memory space.
+  */
+  //   const buffer = new DataView(new ArrayBuffer(length), 0, length);
   const buffer = new ArrayBuffer(length);
-  // To read/write to memory space created by ArrayBuffer,which reference is stored in buffer obj
-  const int8Array = new Int8Array(buffer);
-  //Stores data (value) into a specific position (position value as the index)
-  int8Array[position] = value;
+  const dataView = new DataView(buffer);
 
-  return buffer;
+  /* To read/write to memory space created by ArrayBuffer,which reference is stored in buffer obj.
+  Stores data (value) into a specific position (position value as the index i.e the position the
+    data (value) should be written in). //Sets an 8-bit integer at byte offset position(stores
+    whole numbers within the range of -128 to 127) */
+  dataView.setInt8(position, value);
+  //   const int8Array = new Int8Array(buffer);
+  //   int8Array[position] = value; //Another syntax to achieve this usig the bracket syntax
+
+  return dataView;
 }
