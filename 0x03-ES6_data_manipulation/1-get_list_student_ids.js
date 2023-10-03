d51f7f719fc4449adfd5a0922309c22ffde1c00f
MisturaO <misturaojulari91@gmail.com>
2023-10-03 23:23:27 +0100

    Using map on an array of objs

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -3,7 +3,7 @@ Takes one argument (an array of objects) and this array is the same
 format as getListStudents from the previous task.
 
     If the argument is not an array, the function will return an empty array.
-    Returns an array of ids from a list of objects. 
+    Returns an array of ids from a list of objects.
 */
 export default function getListStudentIds(array) {
   if (!Array.isArray(array)) {
@@ -11,6 +11,6 @@ export default function getListStudentIds(array) {
   }
 
   // Using map to get the value of the first index of the objects and return it in a list
-  const arr = array.map((obj) => Object.values(obj)[0]); 
+  const arr = array.map((obj) => Object.values(obj)[0]);
   return arr;
 }
