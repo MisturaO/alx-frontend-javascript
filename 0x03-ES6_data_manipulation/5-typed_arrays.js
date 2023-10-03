2dad93d7c4c94beb71ddafc481840033d41f75fa
MisturaO <misturaojulari91@gmail.com>
2023-10-04 00:42:25 +0100

    Manipulate buffer data with Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,15 +2,15 @@
 
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    return 'Position outside range';
+    throw new Error('Position outside range');
   }
 
-  const buffer = new DataView(new ArrayBuffer(length), 0, length);
-  //   const buffer = new ArrayBuffer(length);
-  //   const dataView = new DataView(buffer);
+  //   const buffer = new DataView(new ArrayBuffer(length), 0, length);
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
 
-  buffer.setInt8(position, value);
+  dataView.setInt8(position, value);
   //   const int8Array = new Int8Array(buffer);
   //   int8Array[position] = value; //Another syntax to achieve this usig the bracket syntax
-  return buffer;
+  return dataView;
 }
