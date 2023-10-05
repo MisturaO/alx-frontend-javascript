f6886e7e34566c0e177b30369d45eb95e5c6b685
MisturaO <misturaojulari91@gmail.com>
2023-10-05 13:14:38 +0100

    build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,16 @@
+/*Attempts to modify the raedonly attributes after initialization(in the object 
+using the interface) will result in TypeScript compilation errors.
+     This ensures that certain attributes of the interface are immutable once 
+     the object is created, and helps to enforce data integrity and preventing 
+     accidental modification of critical data.
+the '?' defies a optional property*/
+//Teacher interface definition
+export interface Teacher {
+    readonly firstName: string; //should only be modifiable when initialized in obj using the iterf.
+    readonly lastName: string; //only be modifiable when initialized in obj using the iterface.
+    fullTimeEmployee: boolean; //Should always be defined in the object using the interface.
+    yearsOfExperience?: number;//this attribute is optional. You can decide to or not define it.
+    location: string;//This attribute should always be defined in the object using the interface.
+    //index[] signature to add any attribute not specified in the inter in obj using the interface
+    [key: string]: boolean | string | number | any;
+}
